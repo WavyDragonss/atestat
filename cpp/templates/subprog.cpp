448ddfd61a2addcb6eb6fb760c5861438073eeca
WavyDragonss <117162912+WavyDragonss@users.noreply.github.com>
2025-10-24 11:28:48 +0300

    Add subprog.cpp template for user code

---
+++ cpp/templates/subprog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+// --- COD INTRODUS DE UTILIZATOR ---
+{{USER_CODE}}
+// ----------------------------------
+
+// Exemplu de main pentru testare locală (schimbă-l după nevoie)
+int main() {
+    // Aici poți adăuga apeluri către funcțiile scrise de utilizator,
+    // de exemplu: f(5);
+    return 0;
+}
