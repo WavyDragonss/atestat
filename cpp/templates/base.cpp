54670ebc0f9f95434b67663395a1e08dc71f0770
WavyDragonss <117162912+WavyDragonss@users.noreply.github.com>
2025-10-24 11:27:51 +0300

    Create base.cpp

---
+++ cpp/templates/base.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+// --- COD INTRODUS DE UTILIZATOR ---
+{{USER_CODE}}
+// ----------------------------------
+
+int main() {
+    // Exemplu de apel — modifică după nevoie
+    // f(123); // dacă ai definit f
+    return 0;
+}
